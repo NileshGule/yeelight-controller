2a012dafc1d96f9b7d66d2b2d14056079c115bf9
Nilesh Gule <vn_nilesh@yahoo.com>
2020-06-28 23:24:52 +0800

    Removed commented code

--- Program.cs
+++ Program.cs
@@ -11,33 +11,23 @@ namespace YeelightController
         {
             Console.WriteLine("Hello World to AB Room!");
 
+            Device coolLight = new Device("192.168.86.22");
 
-// DeviceLocator.OnDeviceFound += (sender, arg) =>
-//                     {
-//                         WriteLineWithColor($"Device found : {arg.Device}", ConsoleColor.Red);
-//                     };
+            await coolLight.Connect();
 
-//                     List<Device> devices = await DeviceLocator.Discover();
+            Random coolGenerator = new Random(new Random().Next(2, 10));
 
-                    // 192.168.86.22:55443
+            int redValue = coolGenerator.Next(10, 200);
+            int greenValue = coolGenerator.Next(50, 250);
+            int blueValue = coolGenerator.Next(100, 300);
 
-                    Device coolLight = new Device("192.168.86.22");
-                    
-                    await coolLight.Connect();
+            Console.WriteLine($"RedValue : {redValue}");
+            Console.WriteLine($"RedValue : {greenValue}");
+            Console.WriteLine($"RedValue : {blueValue}");
 
-                    Random coolGenerator =new Random(new Random().Next(2,10));
+            await coolLight.SetRGBColor(redValue, greenValue, blueValue);
 
-                    int redValue = coolGenerator.Next(10, 200);
-                    int greenValue = coolGenerator.Next(50,250);
-                    int blueValue = coolGenerator.Next(100,300);
-
-                    Console.WriteLine($"RedValue : {redValue}");
-                    Console.WriteLine($"RedValue : {greenValue}");
-                    Console.WriteLine($"RedValue : {blueValue}");
-
-                    await coolLight.SetRGBColor(redValue, greenValue, blueValue);
-
-                    await coolLight.SetBrightness(coolGenerator.Next(50, 100), coolGenerator.Next(100, 3000));
+            await coolLight.SetBrightness(coolGenerator.Next(50, 100), coolGenerator.Next(100, 3000));
 
         }
 
